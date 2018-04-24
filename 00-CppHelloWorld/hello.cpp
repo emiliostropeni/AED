3dc3e3f8c22b58efd39315136b1c6646d4ae5896
emiliostropeni <38562256+emiliostropeni@users.noreply.github.com>
2018-04-24 14:59:56 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,20 @@
+// First Hello World for AED
+/*K1051
+Emilio Stropeni
+161545-2
+2018
+Ing. Sola
+*/
+
+//inicio de codigo
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	cout << "Hello World!\n";
+
+    return 0;
+}
