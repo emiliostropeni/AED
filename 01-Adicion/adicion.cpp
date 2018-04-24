f82ae2f97aec7887b16f260abe8bdc0d7a26f0ee
emiliostropeni <38562256+emiliostropeni@users.noreply.github.com>
2018-04-24 15:19:49 -0300

    Create adicion.cpp

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,28 @@
+// Suma de dos Naturales
+/*K1051
+Emilio Stropeni
+161545-2
+2018
+Ing. Sola
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	//definicion de variables
+	int a;
+	int b;
+
+	//ingreso de datos
+	cout << "Ingrese el primer numero: \n";
+	cin >> a;
+
+	cout << "Ingrese el segundo numero: \n";
+	cin >> b;
+
+	//muestra de resultado
+	cout << "Su resultado es: " << a + b << "\n";
+	
+}
